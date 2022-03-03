7e2e15772092fd75cc67676a9cc24583f09cf22d
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-03-02 19:38:04 -0800

    updated up to organizing express code

--- src/app.js
+++ src/app.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const app = express();
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+const flipsRouter = require("./flips/flips.router");
+const counts = require("./data/counts-data");
+
+app.use(express.json());
+
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next({
+      status: 404,
+      message: `Count id not found: ${countId}`,
+    });
+  } else {
+    res.json({ data: foundCount });
+  }
+});
+
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+});
+
+
+app.use("/flips", flipsRouter);
 
 // Not found handler
-app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
 });
 
 // Error handler
-app.use((error, request, response, next) => {
-  console.error(error);
-  response.send(error);
+app.use((err, req, res, next) => {
+  console.error(err);
+  const { status = 500, message = "Something went wrong!" } = err;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
